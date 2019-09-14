dd4827f3fe6a3778e43b5e7f74b259ca87ab7413
Michal Pierzchlewicz <mike@teacode.io>
2019-09-14 21:36:01 +0200

    place select

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -3,11 +3,13 @@ import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 import AgeConfirmationScreen from '../Containers/AgeConfirmationScreen'
+import PlaceSelection from '../Containers/PlaceSelection'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
   LaunchScreen: { screen: LaunchScreen },
-  AgeConfirmationScreen: {screen: AgeConfirmationScreen}
+  AgeConfirmationScreen: {screen: AgeConfirmationScreen},
+  PlaceSelection: { screen: PlaceSelection },
 }, {
   // Default config for all screens
   headerMode: 'none',
